8351abc71680cde056342ddd5783327197a4dea1
vectronic <hello.vectronic@gmail.com>
2020-07-16 00:20:04 +0100

    fix: support update to cli-framework 5.4.0

--- src/exampleCli.js
+++ src/exampleCli.js
@@ -1,5 +1,40 @@
-import { NodeCLI, PLUGIN_REGISTRY_SERVICE } from '@flowscripter/cli-framework';
-import ExampleCommandFactory from './ExampleCommandFactory';
+import {
+    PLUGIN_REGISTRY_SERVICE,
+    STDOUT_PRINTER_SERVICE,
+    BaseNodeCLI,
+    MultiCommandHelpGlobalCommand,
+    MultiCommandHelpSubCommand,
+    PrompterService,
+    StderrPrinterService,
+    StdoutPrinterService,
+    UsageCommand,
+    VersionCommand
+} from '@flowscripter/cli-framework';
+
+const helpGlobalCommand = new MultiCommandHelpGlobalCommand();
+const usageCommand = new UsageCommand(helpGlobalCommand);
+
+function getCommand() {
+    return {
+        name: 'greeter',
+        description: 'The classic example',
+        options: [{
+            name: 'subject',
+            defaultValue: 'world',
+            description: 'Who to greet',
+            shortAlias: 's',
+            isOptional: true
+        }],
+        positionals: [],
+        run: async (commandArgs, context) => {
+            const printer = context.serviceRegistry.getServiceById(STDOUT_PRINTER_SERVICE);
+            if (printer == null) {
+                throw new Error('STDOUT_PRINTER_SERVICE not available in context');
+            }
+            printer.info(`Hello ${commandArgs.subject}\n`);
+        }
+    };
+}
 
 (async () => {
     // Provide PluginRegistry custom config to use a custom package install location
@@ -10,9 +45,16 @@ import ExampleCommandFactory from './ExampleCommandFactory';
         pluginLocation: '/tmp/js-example-cli-plugins'
     });
 
-    const nodeCli = new NodeCLI('js-example-cli', serviceConfigs, new Map());
-
-    nodeCli.addCommandFactory(new ExampleCommandFactory());
+    const nodeCli = new BaseNodeCLI([
+        new StderrPrinterService(90),
+        new StdoutPrinterService(90),
+        new PrompterService(90)
+    ], [
+        helpGlobalCommand,
+        new MultiCommandHelpSubCommand(),
+        new VersionCommand(),
+        getCommand()
+    ], serviceConfigs, new Map(), 'js-example-cli', usageCommand, usageCommand);
 
     await nodeCli.execute();
 })();
